1afea9e746592ca755fbf8bfb8b9768ed125fc3d
Bin Wu <binwu@google.com>
2019-11-13 12:26:14 +0000

    added error handling output

--- readme.md
+++ readme.md
@@ -4,4 +4,6 @@ You can use this master branch as a skeleton java project
 
 ### Proposed streaming pipeline
 
+#### IMPORTANT: in the code example, assume the pubsub message is csv text encoded in utf-8
+
 pubsub -> dataflow -> gcs(avro), bq(table), gcs(csv, deadletter)
--- src/main/java/bindiego/BindiegoStreaming.java
+++ src/main/java/bindiego/BindiegoStreaming.java
@@ -7,6 +7,10 @@ import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.AvroIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposition;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition;
+import org.apache.beam.sdk.io.FileBasedSink;
 import org.apache.beam.sdk.metrics.Counter;
 import org.apache.beam.sdk.metrics.Distribution;
 import org.apache.beam.sdk.metrics.Metrics;
@@ -27,38 +31,86 @@ import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.DoFn.MultiOutputReceiver;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.beam.sdk.values.TupleTagList;
 
 import org.joda.time.Duration;
 
+import bindiego.io.WindowedFilenamePolicy;
 import bindiego.utils.DurationUtils;
 
 public class BindiegoStreaming {
     /* extract the csv payload from message */
     public static class ExtractPayload extends DoFn<PubsubMessage, String> {
         @ProcessElement
-        public void processElement(ProcessContext ctx) throws IllegalArgumentException {
-            PubsubMessage psmsg = ctx.element();
-            
-            ctx.output(new String(psmsg.getPayload(), StandardCharsets.UTF_8));
+        public void processElement(ProcessContext ctx, MultiOutputReceiver r) 
+                throws IllegalArgumentException {
+
+            String str = null;
+
+            try {
+                PubsubMessage psmsg = ctx.element();
+                str = new String(psmsg.getPayload(), StandardCharsets.UTF_8);
+
+                r.get(STR_OUT).output(str);
+            } catch (Exception ex) {
+                if (null == str)
+                    str = "AUTO_MSG failed to extract message";
+                r.get(STR_FAILURE_OUT).output(str);
+            }
         }
     }
 
     static void run(BindiegoStreamingOptions options) {
         Pipeline p = Pipeline.create(options);
 
-        p.apply("Read Pubsub Events", 
-                PubsubIO.readMessagesWithAttributesAndMessageId()
-                    .fromSubscription(options.getSubscription()))
-            .apply("Extract CSV payload from pubsub message",
-                ParDo.of(new ExtractPayload()))
-            .apply(Window.into(FixedWindows.of(DurationUtils.parseDuration(options.getWindowSize()))))
-            .apply("Write windowd CSV files", 
-                TextIO.write()
-                    .to(options.getOutputDir())
-                    .withNumShards(options.getNumShards())
-                    .withWindowedWrites());
+        PCollection<PubsubMessage> messages = p.apply("Read Pubsub Events", 
+            PubsubIO.readMessagesWithAttributesAndMessageId()
+                .fromSubscription(options.getSubscription()));
+
+        PCollectionTuple processedData = messages.apply("Extract CSV payload from pubsub message",
+            ParDo.of(new ExtractPayload())
+                .withOutputTags(STR_OUT, TupleTagList.of(STR_FAILURE_OUT)));
+
+        PCollection<String> healthData = processedData.get(STR_OUT)
+            .apply(options.getWindowSize() + " window for healthy data",
+                Window.into(FixedWindows.of(DurationUtils.parseDuration(options.getWindowSize()))));
+
+        PCollection<String> errData = processedData.get(STR_FAILURE_OUT)
+            .apply(options.getWindowSize() + " window for error data",
+                Window.into(FixedWindows.of(DurationUtils.parseDuration(options.getWindowSize()))));
+
+        healthData.apply("Write windowd healthy CSV files", 
+            TextIO.write()
+                .withNumShards(options.getNumShards())
+                .withWindowedWrites()
+                .to(
+                    new WindowedFilenamePolicy(
+                        options.getOutputDir(),
+                        options.getFilenamePrefix(),
+                        options.getOutputShardTemplate(),
+                        options.getCsvFilenameSuffix()
+                    ))
+                .withTempDirectory(
+                    FileBasedSink.convertToFileResourceIfPossible(options.getTempLocation())));
+
+        errData.apply("Write windowd error data in CSV format", 
+            TextIO.write()
+                .withNumShards(options.getNumShards())
+                .withWindowedWrites()
+                .to(
+                    new WindowedFilenamePolicy(
+                        options.getErrOutputDir(),
+                        options.getFilenamePrefix(),
+                        options.getOutputShardTemplate(),
+                        options.getCsvFilenameSuffix()
+                    ))
+                .withTempDirectory(
+                    FileBasedSink.convertToFileResourceIfPossible(options.getTempLocation())));
 
         p.run();
         //p.run().waitUntilFinish();
@@ -79,6 +131,26 @@ public class BindiegoStreaming {
         @Required
         ValueProvider<String> getOutputDir();
         void setOutputDir(ValueProvider<String> value);
+
+        @Description("The directory to output error files to. Must end with a slash.")
+        @Required
+        ValueProvider<String> getErrOutputDir();
+        void setErrOutputDir(ValueProvider<String> value);
+
+        @Description("File name prefix.")
+        @Default.String("bindiego")
+        ValueProvider<String> getFilenamePrefix();
+        void setFilenamePrefix(ValueProvider<String> value);
+
+        @Description("CSV File name suffix.")
+        @Default.String(".csv")
+        ValueProvider<String> getCsvFilenameSuffix();
+        void setCsvFilenameSuffix(ValueProvider<String> value);
+
+        @Description("Avro File name suffix.")
+        @Default.String(".avro")
+        ValueProvider<String> getAvroFilenameSuffix();
+        void setAvroFilenameSuffix(ValueProvider<String> value);
 
         @Default.String("W-P-SS-of-NN")
         ValueProvider<String> getOutputShardTemplate();
@@ -102,4 +174,11 @@ public class BindiegoStreaming {
 
         run(options);
     }
+
+    /* tag for main output when extracting pubsub message payload*/
+    private static final TupleTag<String> STR_OUT = 
+        new TupleTag<String>() {};
+    /* tag for failure output from the UDF */
+    private static final TupleTag<String> STR_FAILURE_OUT = 
+        new TupleTag<String>() {};
 }
