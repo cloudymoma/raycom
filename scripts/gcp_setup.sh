e01774c1f0124d5ee9fad52ff5dfe2c0102bfb0e
Bin Wu <binwu@google.com>
2020-06-05 14:27:56 +0000

    updated GCP stackdriver sink settings

--- scripts/gcp_setup.sh
+++ scripts/gcp_setup.sh
@@ -8,12 +8,13 @@ sink=gclb-sink-pubsub
 # create a pubsub topic
 gcloud pubsub topics create $topic
 
-# create subscription 
+# create subscription
 gcloud pubsub subscriptions create $subscription --topic=$topic --topic-project=$project
 
 # create a stackdriver sink (pubsub)
 gcloud logging sinks create $sink pubsub.googleapis.com/projects/$project/topics/$topic \
-    --log-filter='resource.type="http_load_balancer"'
+    --log-filter='resource.type="http_load_balancer" AND NOT httpRequest.requestUrl:ingest'
+    # --log-filter='resource.type="http_load_balancer"'
 
 # add service account used by stackdriver for pubsub topic
 logging_sa=`gcloud logging sinks describe $sink | grep "writerIdentity" | cut -d ' ' -f 2`
