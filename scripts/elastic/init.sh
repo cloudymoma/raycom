5e66fdfcf022ea13f4d2f17de902ff7493b455b3
Bin Wu <binwu@google.com>
2021-01-28 07:24:28 +0000

    fixed a minor bug for elasticsearch init script for user authentication

--- scripts/elastic/init.sh
+++ scripts/elastic/init.sh
@@ -10,7 +10,7 @@ es_pass=changeme
 # Create an ES pipeline for GCLB logs
 __create_index_pipeline() {
     curl -X PUT \
-        -u "elastic:${es_pass}" \
+        -u "${es_user}:${es_pass}" \
         "${es_client}/_ingest/pipeline/gclb" \
         -H "Content-Type: application/json" \
         -d "@${pwd}/index-gclb-pipeline.json"
@@ -19,42 +19,42 @@ __create_index_pipeline() {
 # create an ES template
 __create_index_template() {
     curl -X PUT \
-        -u "elastic:${es_pass}" \
+        -u "${es_user}:${es_pass}" \
         "${es_client}/_template/gclb" \
         -H "Content-Type: application/json" \
         -d "@${pwd}/index-gclb-template.json"
 
     # veryfy
     curl -X GET \
-        -u "elastic:${es_pass}" \
+        -u "${es_user}:${es_pass}" \
         "${es_client}/_template/gclb"
 }
 
 __create_index_and_setup() {
     # create a lifecycle pocily, edit the json data file according to your needs
     curl -X PUT \
-        -u "elastic:${es_pass}" \
+        -u "${es_user}:${es_pass}" \
         "${es_client}/_ilm/policy/gclb-policy" \
         -H "Content-Type: application/json" \
         -d "@${pwd}/index-gclb-policy.json"
 
     # create an index and assign an alias for writing
     curl -X PUT \
-        -u "elastic:${es_pass}" \
+        -u "${es_user}:${es_pass}" \
         "${es_client}/gclb-000001" \
         -H "Content-Type: application/json" \
         -d '{"aliases": {"gclb-ingest": { "is_write_index": true }}}'
 
     # veryfy
     curl -X GET \
-        -u "elastic:${es_pass}" \
+        -u "${es_user}:${es_pass}" \
         "${es_client}/gclb*/_ilm/explain"
 }
 
 # create a Kibana index pattern
 __create_index_pattern() {
     curl -X POST \
-        -u "elastic:${es_pass}" \
+        -u "${es_user}:${es_pass}" \
         "${kbn_host}/api/saved_objects/index-pattern" \
         -H "kbn-xsrf: true" \
         -H "Content-Type: application/json" \
